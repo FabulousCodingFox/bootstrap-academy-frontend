8c9b9c435505548654db5c8b64bdfdea8cb89824
Fabian F <78906517+FabulousCodingFox@users.noreply.github.com>
2024-10-27 10:36:11 +0100

    fix: adjusting z index of NavbarDrawer.vue

--- tailwind.config.js
+++ tailwind.config.js
@@ -286,8 +286,7 @@ module.exports = {
         },
         // transition duration-300 ease-out
         '.transition-basic': {
-          transitionProperty:
-            'color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter',
+          transitionProperty: 'color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter',
           transitionTimingFunction: 'cubic-bezier(0.4, 0, 0.2, 1)',
           transitionDuration: '300ms',
         },
